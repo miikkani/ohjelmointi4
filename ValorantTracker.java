d3abb28afefcf196e44ffb4c4e46e6df6cbc3424
Miikka Niemi <niemimi@paju.oulu.fi>
2021-04-13 18:08:28 +0300

    Update main class

--- ValorantTracker.java
+++ ValorantTracker.java
@@ -1,3 +1,36 @@
-public class ValorantTracker {
+import javax.swing.*;
 
+/**
+ * ValorantTracker. Epic Application #1.
+ * This is the main class for application.
+ *
+ */
+public class ValorantTracker {
+     ValorantTracker() {
+
+         JFrame jfrm  = new JFrame("ValorantTracker");
+         jfrm.setSize(400,150);
+         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+         JLabel test = new JLabel("Win % 100!");
+
+         jfrm.add(test);
+
+         jfrm.setVisible(true);
+
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+         SwingUtilities.invokeLater(new Runnable() {
+             public void run() {
+                 new ValorantTracker();
+             }
+         });
+
+    }
 }
