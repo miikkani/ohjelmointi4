1a69cc2c8e3881014e5461db0dcb867f60626cb9
Miikka Niemi <niemimi@paju.oulu.fi>
2021-04-21 17:32:19 +0300

    Create class for calculating percentages.

---
+++ vtracker/data/StatsBuilder.java
@@ -0,0 +1,63 @@
+package vtracker.data;
+
+
+/**     !!Work In Progress!!
+ *
+ * This class calculates statistics from given match database.
+ *
+ * TODO:
+ *  - Make this a factory class etc.?
+ *
+ */
+public class StatsBuilder {
+    private double totalWP;
+
+
+    /**
+     * Maybe obsolete.
+     */
+    public StatsBuilder() {
+
+    }
+
+
+    /**
+     * Maybe...
+     */
+    public double calculateWinPercent() {
+
+        return 0.0;
+    }
+
+
+    /**
+     * Calculates all percentages and updates internal data
+     * structures.
+     *
+     */
+    public void updateStats() {
+
+
+    }
+
+
+    /**
+     * Maybe...
+     */
+    public double getTotalWinPercentage() {
+        return totalWP;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
