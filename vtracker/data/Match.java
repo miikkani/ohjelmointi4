0edb75a4f3437bc5675793756ab5f89d791bfc17
Miikka Niemi <niemimi@paju.oulu.fi>
2021-05-12 15:03:32 +0300

    Add timestamp

--- vtracker/data/Match.java
+++ vtracker/data/Match.java
@@ -1,21 +1,25 @@
 package vtracker.data;
 
+
 public class Match {
     private final String agent;
     private final MatchResult result;
+    private final long timestamp;
 
     public Match(String agent, MatchResult result) {
         this.agent = agent;
         this.result = result;
+        timestamp = System.currentTimeMillis();
     }
 
 
     public String getAgent(){return this.agent;}
     public MatchResult getResult(){return this.result;}
+    public long getTimestamp(){return this.timestamp;}
 
     @Override
     public String toString() {
-        return this.hashCode()
+        return timestamp
                 + ":" + agent
                 + ":" + result;
     }
