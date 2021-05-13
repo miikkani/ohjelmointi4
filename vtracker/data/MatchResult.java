50592f6d7c5a86a7fb0ddf661dc01abf0eafacad
Miikka Niemi <niemimi@paju.oulu.fi>
2021-05-13 15:34:41 +0300

    Write comments for MatchResult

--- vtracker/data/MatchResult.java
+++ vtracker/data/MatchResult.java
@@ -1,7 +1,19 @@
 package vtracker.data;
 
+/**
+ * Enumeration representing possible results of a single match.
+ */
 public enum MatchResult {
+    /**
+     * Match resulted as a win.
+     */
     WIN,
+    /**
+     * Match resulted as a loss.
+     */
     LOSS,
+    /**
+     * Match resulted as a draw.
+     */
     DRAW
 }
